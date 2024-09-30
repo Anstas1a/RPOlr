098e99f303f127756604cf9b19dbf9a552ab653a
Anastasia <80666996+Anstas1a@users.noreply.github.com>
2024-09-30 17:39:52 +0300

    Update main.dart
    add view screen for 4 lab

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,40 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:helloworld/bloc/notes_bloc.dart';
 import 'package:helloworld/screen/async_screen.dart';
 import 'package:helloworld/screen/second_lab_screen.dart';
 import 'package:helloworld/screen/todo_list_screen.dart';
+import 'package:helloworld/screen/notes_screen.dart';
 
-void main() => runApp(MyApp());
+import 'package:firebase_core/firebase_core.dart';
+import 'package:helloworld/services/firebase_manager.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const ButtonScreen(),
+    return BlocProvider(
+      create: (context) => NotesBloc(FirebaseManager())..add(LoadNotesEvent()),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const ButtonScreen(),
+      ),
     );
   }
 }
@@ -61,9 +86,30 @@ class ButtonScreen extends StatelessWidget {
               },
               child: const Text('Async Lab'),
             ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const NotesListScreen(),
+                  ),
+                );
+              },
+              child: const Text('Notes Lab'),
+            ),
           ],
         ),
       ),
     );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    final client = super.createHttpClient(context);
+    client.badCertificateCallback =
+        (X509Certificate cert, String host, int port) => true;
+    return client;
   }
 }

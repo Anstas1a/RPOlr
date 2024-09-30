7dae574898d1e37f9d5731ef2280f2a319a48bbe
Anastasia <80666996+Anstas1a@users.noreply.github.com>
2024-09-30 14:21:22 +0300

    Update main.dart
    Add link to second lab

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:helloworld/screen/second_lab_screen.dart';
 import 'package:helloworld/screen/todo_list_screen.dart';
 
 void main() => runApp(MyApp());
@@ -7,7 +8,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TodoListScreen(),
+      home: const ButtonScreen(),
+    );
+  }
+}
+
+class ButtonScreen extends StatelessWidget {
+  const ButtonScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Labs Screen'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const TodoListScreen(),
+                  ),
+                );
+              },
+              child: const Text('Todo List'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SecondLabScreen(),
+                  ),
+                );
+              },
+              child: const Text('Second Lab'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
